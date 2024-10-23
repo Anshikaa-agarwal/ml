67af4243063c126ab2217eb8af3d4a34a821fbf9
Anshika <anshika2020ag@gmail.com>
2024-10-23 23:47:27 +0530

    seaborn

---
+++ python_basics/seabornIntro.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# seaborn has some inbuilt datasets
+
+# 1st dataset: total bill vs total tip
+# scatter graph
+tips = sns.load_dataset('tips')
+print(tips)
+sns.scatterplot(x='total_bill', y='tip', data=tips, hue='sex', size='size', style='time')
+plt.show()
+
+# bar graph
+sns.barplot(data=tips, x='day', y='tip')
+plt.show()
+
+# histogram
+sns.histplot(tips['total_bill'], kde=True)  # KDE shows the data's probability density
+plt.show()
